c15268da7825009ea8d6a65a5b4afe7eed47bf3b
SohamShah20 <sohamjshah20@gmail.com>
2024-10-30 19:00:50 +0530

    Page Layout #1
    Added empty pages and routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import { NavLink, Routes, Route, Link } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import About from './pages/About';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+import Dashboard from './pages/Dashboard';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar />
+
+      <Routes>
+        <Route path = "/" element = {<Home />} />
+        <Route path = "/about" element = {<About />} />
+        <Route path = "/login" element = {<Login />} />
+        <Route path = "/signup" element = {<Signup />} />
+        <Route path = "/dashboard" element = {<Dashboard />} />
+      </Routes>
     </div>
   );
 }
