ab8109dcd22dd3782c8143732cf6f7a398c3ddb2
SohamShah20 <sohamjshah20@gmail.com>
2024-11-11 19:53:40 +0530

    View Feedbacks
    -Dealer can view feedbacks (incomplete)

--- Backend/routers/dealerRouter.js
+++ Backend/routers/dealerRouter.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import {getrequests,acceptrequests,getacceptedrequests,genreceipt,getclosedrequests} from '../controllers/dealerController.js';
+import {getrequests,acceptrequests,getacceptedrequests,genreceipt,getclosedrequests,getfeedbacks} from '../controllers/dealerController.js';
 import express from "express";
 import { verifyToken } from "../controllers/Verifyuser.js";
 const router = express.Router();
@@ -9,4 +9,5 @@ router.put('/acceptrequests/:id',verifyToken,acceptrequests);
 router.post('/genreceipt/:id',verifyToken,genreceipt);
 router.get('/getacceptedrequests/:id',getacceptedrequests);
 router.get('/getclosedrequests/:id',getclosedrequests);
+router.get('/getfeedbacks/:id', getfeedbacks);
 export default router;
